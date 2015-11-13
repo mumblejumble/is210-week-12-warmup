f1041c79b808b4bf6f6b3de6b39e981d0258283e
mumblejumble <danqing.lin@spsmail.cuny.edu>
2015-11-13 23:58:37 +0000

    LookupError task_01.py

--- task_01.py
+++ task_01.py
@@ -4,4 +4,15 @@
 
 
 def simple_lookup(var1, var2):
-    return var1[var2]
+    """Exception handling
+
+    Examples:
+        >>> simple_lookup([1,2],4)
+        Warning: Your index/key doesn't exist.
+        [1, 2]
+    """
+    try:
+        return var1[var2]
+    except LookupError:
+        print 'Warning: Your index/key doesn\'t exist. \n{}'
+        return var1
