30b398bbccdc728cb8bbaf288a628540892b307e
mumblejumble <danqing.lin@spsmail.cuny.edu>
2015-11-15 04:28:23 +0000

    documentation added

--- task_02.py
+++ task_02.py
@@ -12,7 +12,29 @@ class InvalidAgeError(Exception):
 
 
 def get_age(birthyear):
-    """Function that tests age"""
+    """Function that tests age
+
+    Args:
+        birthyear(int): birthyeat being passed in.
+
+    Attributes:
+        age(int): calculates age.
+
+    Returns:
+        age if no error, otherwise error.
+
+    Examples:
+        >>> get_age(1989)
+        26
+        >>> get_age(2016)
+
+        Traceback (most recent call last):
+        File "<pyshell#1>", line 1, in <module>
+          get_age(2016)
+        File "/home/vagrant/Desktop/is210-week-12-warmup/task_02.py", line 27,
+        in get_age raise InvalidAgeError()
+        InvalidAgeError
+    """
     age = datetime.datetime.now().year - birthyear
     if age < 0:
         raise InvalidAgeError()
